13ab6e857e62da5d6b03fbf85ad150d230e7ddf1
kiat <kiat@cs.utexas.edu>
2022-03-12 16:04:29 -0600

    added an example calculation of EMA

--- python/example.py
+++ python/example.py
@@ -23,23 +23,22 @@ with grpc.insecure_channel('challenge.msrg.in.tum.de:5023', options=op) as chann
 
     #Step 1 - Create a new Benchmark
     benchmarkconfiguration = ch.BenchmarkConfiguration(token='zqultcyalnowfgxjlzlsztkcquycninr'.strip(),
-                                                       batch_size=3_000,
-                                                       benchmark_name="this name shows_up_in_dashboard",
+                                                       benchmark_name="group 17",
                                                        benchmark_type="test",
-                                                       queries=[ch.BenchmarkConfiguration.Query.Q1, ch.BenchmarkConfiguration.Query.Q2])
+                                                       queries=[ch.Query.Q1, ch.Query.Q2])
     benchmark = stub.createNewBenchmark(benchmarkconfiguration)
 
     #Step 2 - get all locations
     # The locations depend on the benchmark_type
-    loc = stub.getLocations(benchmark) #get all the locations
-    print("Fetched %s locations" % (len(loc.locations)))
+    # loc = stub.getLocations(benchmark) #get all the locations
+    # print("Fetched %s locations" % (len(loc.locations)))
 
 
     #Step 3 (optional) - Calibrate the latency
-    ping = stub.initializeLatencyMeasuring(benchmark)
-    for i in range(10):
-        ping =stub.measure(ping)
-    stub.endMeasurement(ping)
+    # ping = stub.initializeLatencyMeasuring(benchmark)
+    # for i in range(10):
+    #    ping =stub.measure(ping)
+    # stub.endMeasurement(ping)
 
     #Step 4 - Start Eventprocessing and start the clock
     stub.startBenchmark(benchmark)
@@ -48,39 +47,75 @@ with grpc.insecure_channel('challenge.msrg.in.tum.de:5023', options=op) as chann
     cnt_current = 0
     cnt_historic = 0
     cnt = 0
-
-    batch = stub.nextBatch(benchmark)
-    while batch:
-        cnt_current += len(batch.current)
-        cnt_historic += len(batch.lastyear)
-
-        if(cnt % 100) == 0:
-            ts_str = ""
-            if len(batch.current) > 0: #it could happen that only events from lastyear are available
-                ts = batch.current[0].timestamp
-                dt = datetime.utcfromtimestamp(ts.seconds)
-                ts_str = dt.strftime("%Y-%m-%d %H:%M:%S.%f")
-
-            print("processed %s - current_time: %s, num_current: %s, num_historic: %s, total_events: %s" % (cnt, ts_str, cnt_current, cnt_historic, ( cnt_current + cnt_historic)))
+    event_count = 0 
 
 
-        # result_payload_q1 = processTheBatchQ1(batch) #here is your implementation ;)
-        resultQ1 = ch.ResultQ1(benchmark_id=benchmark.id,  #The id of the benchmark
-                               batch_seq_id=batch.seq_id,
-                               topkimproved=[])
+    while True:
+        batch = stub.nextBatch(benchmark)
+        event_count = event_count + len(batch.events)
 
-        stub.resultQ1(resultQ1) #send the result of query 1, also send the result of Q2 in case you calculate both
+        def queryResults(symbols: list[str]) -> list[ch.Indicator]:
+            # Your part: calculate the indicators for the given symbols
+            return list()
 
-        # processTheBatchQ1(batch) # here should be the implementation of Q2
-        resultQ2 = ch.ResultQ2(benchmark_id=benchmark.id, batch_seq_id=batch.seq_id, histogram=[])
-        stub.resultQ2(resultQ2)
+        resultQ1 = ch.ResultQ1(
+            benchmark_id=benchmark.id,  # The id of the benchmark
+            batch_seq_id=batch.seq_id,  # The sequence id of the batch
+            indicators=queryResults(batch.lookup_symbols))
+        stub.resultQ1(resultQ1)  # send the result of query 1 back
 
-        if batch.last or cnt > 1_000: #here we just stop after 1000 so we see a result, this is allowed only for testing
+        def crossoverEvents() -> list[ch.CrossoverEvent]:
+            # Your part: calculate the crossover events
+            return list()
+
+        # do the same for Q2
+        resultQ2 = ch.ResultQ2(
+            benchmark_id=benchmark.id,  # The id of the benchmark
+            batch_seq_id=batch.seq_id,  # The sequence id of the batch
+            crossover_events=crossoverEvents())
+
+        stub.resultQ2(resultQ2)  # submit the results of Q2
+
+        # Step 4 - once the last event is received, stop the clock
+        # See the statistics within ~5min here: https://challenge.msrg.in.tum.de/benchmarks/
+        if batch.last:
+            print(f"received last batch, total batches: {event_count}")
+            stub.endBenchmark(benchmark)
             break
 
-        cnt = cnt + 1
-        batch = stub.nextBatch(benchmark)
 
-    stub.endMeasurement(benchmark)
+#     batch = stub.nextBatch(benchmark)
+#     while batch:
+#         cnt_current += len(batch.current)
+#         cnt_historic += len(batch.lastyear)
 
-print("finished")
+#         if(cnt % 100) == 0:
+#             ts_str = ""
+#             if len(batch.current) > 0: #it could happen that only events from lastyear are available
+#                 ts = batch.current[0].timestamp
+#                 dt = datetime.utcfromtimestamp(ts.seconds)
+#                 ts_str = dt.strftime("%Y-%m-%d %H:%M:%S.%f")
+
+#             print("processed %s - current_time: %s, num_current: %s, num_historic: %s, total_events: %s" % (cnt, ts_str, cnt_current, cnt_historic, ( cnt_current + cnt_historic)))
+
+
+#         # result_payload_q1 = processTheBatchQ1(batch) #here is your implementation ;)
+#         resultQ1 = ch.ResultQ1(benchmark_id=benchmark.id,  #The id of the benchmark
+#                                batch_seq_id=batch.seq_id,
+#                                topkimproved=[])
+
+#         stub.resultQ1(resultQ1) #send the result of query 1, also send the result of Q2 in case you calculate both
+
+#         # processTheBatchQ1(batch) # here should be the implementation of Q2
+#         resultQ2 = ch.ResultQ2(benchmark_id=benchmark.id, batch_seq_id=batch.seq_id, histogram=[])
+#         stub.resultQ2(resultQ2)
+
+#         if batch.last or cnt > 1_000: #here we just stop after 1000 so we see a result, this is allowed only for testing
+#             break
+
+#         cnt = cnt + 1
+#         batch = stub.nextBatch(benchmark)
+
+#     stub.endMeasurement(benchmark)
+
+# print("finished")
