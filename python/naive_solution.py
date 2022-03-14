15252cf82760a5f141b513770e8bc66c88a287c7
Kevin Li <likevinsen@gmail.com>
2022-03-14 18:53:18 -0500

    remove unused code

--- python/naive_solution.py
+++ python/naive_solution.py
@@ -5,93 +5,6 @@ from google.protobuf.timestamp_pb2 import Timestamp
 
 import messages.challenger_pb2 as ch
 
-
-def compute_ema(
-        events: List[ch.Event],
-        previous: Dict[str, List[float]]
-    ) -> Tuple[List[ch.Indicator], Dict[str, List[float]]]:
-    """
-    Computes the EMAs for the current batch/window and returns the list of indicators 
-    + updated dictionary.
-
-    Args:
-        events (List[ch.Event]): Events from batch.
-        previous (Dict[str, List[float]]): EMAs previous previous time window (i - 1).
-
-    Returns:
-        List[ch.Indicator]: List of indicators.
-        Dict[str, List[float]]: Updated EMAs for current time window. First element is j = 38, 
-                                second element is j = 100.
-    """
-    
-    weighted_first = lambda closing, j: closing * (2 / (1 + j))
-    weighted_second = lambda prev_w, j: prev_w * (1 - (2 / (1 + j)))
-    ema_j = lambda closing, prev_w, j: weighted_first(closing, j) + weighted_second(prev_w, j)
-
-    cur = defaultdict(list)
-    indicators = []
-    for event in events:
-        symbol = event.symbol
-
-        prev_38, prev_100 = 0, 0 if symbol not in previous else previous[symbol]
-        
-        ema_38 = ema_j(event.last_trade_price, prev_38, 38)
-        ema_100 = ema_j(event.last_trade_price, prev_100, 100)
-        
-        indicators.append(ch.Indicator(symbol=event.symbol, ema_38=ema_38, ema_100=ema_100))
-        
-        cur[symbol] = [ema_38, ema_100]
-    return indicators, cur
-            
-
-
-def crossover_events(
-        symbols: List[str],
-        cur: Dict[str, List[float]],
-        prev: Dict[str, List[float]],
-        events: List[ch.Event]
-    ) -> List[ch.CrossoverEvent]:
-    """Detects cross over events: bullish or bearish based on two EMAs with j: 38, 100.
-
-    Args:
-        symbols (List[str]): Financial instrument symbols for this batch/window.
-        cur (Dict[List[float]]): EMAs for window i
-        prev (Dict[List[float]]): EMAs for window i - 1
-
-    Returns:
-        List[ch.CrossoverEvent]: List of crossover events (signal type, security type, timestamp, symbol).
-    """
-    
-    crossovers = []
-    for s in symbols:
-        
-        # No event if not in previous or current window.
-        if s not in prev or s not in cur:
-            continue
-        
-        prev_w = prev[s]
-        cur_w = cur[s]
-
-        # Trigger buy.
-        if prev_w[0] <= prev_w[1] and cur_w[0] > cur_w[1]:
-            pass
-        
-        # TODO: get security types without going through all events again.
-    
-    return crossovers
-
-
-
-# events {
-#   symbol: "IGAGG.FR"
-#   security_type: Index
-#   last_trade_price: 53.08000183105469
-#   last_trade {
-#     seconds: 1636354875.155
-#     nanos: 
-#   }
-# }
-
 def max_events(e1: ch.Event, e2: ch.Event) -> ch.Event:
     if e1.last_trade.seconds > e2.last_trade.seconds:
         return e1
@@ -185,8 +98,6 @@ def main():
 
     event_count = 0
     batch_count = 0
-    
-    previous = defaultdict(list)
 
     trackers = {}
     
